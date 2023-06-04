f3aa69a8d7ffeb807890b224dfcf99308dcebf5f
nitinkalra <nitin.kalra70@gmail.com>
2023-06-04 14:20:38 +0530

    added requirments

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+with open( "README.md", 'r', encoding= 'utf-8') as f:
+    long_description = f.read()
+
+__version__ = "0,0,0,0"
+
+REPO_NAME = "Text-Summarizer-project"
+AUTHOR_USER_NAME = "nitin kalra"
+SRC_REPO = 'textsummarizer'
+AUTHOR_EMAIL = "nitin.kalra70@gmail.com"
